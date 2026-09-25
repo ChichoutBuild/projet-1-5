71f13d6c52830bbcce2c068da9d64d09d52fd8ff
ChichoutBuild <111909991+ChichoutBuild@users.noreply.github.com>
2026-09-25 23:44:18 +0200

    Create page.js

---
+++ app/tableau-de-bord/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function TableauDeBord() {
+  return (
+    <main className="page">
+      <h1 className="titre">Tableau de bord</h1>
+      <p className="texte">En construction... 🛠️</p>
+    </main>
+  );
+}
